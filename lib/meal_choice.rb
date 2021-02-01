90a0ea7a3d63d84fa6cd18dd56650db737d3b4a6
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-01 19:17:03 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 protein = "Meat"
 
-def meal_choice (protein = "tofu", "broccoli", "string beans")
+def meal_choice (broccoli, string beans, protein = "tofu")
 puts "What a nutritious meal!"
 meal = "A plate of #{protein} with #{veg1} and #{veg2}."
 puts meal
