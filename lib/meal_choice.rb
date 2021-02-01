dc8cdf0a84dcccb3192ae4006e7989493d6090b5
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-01 18:59:19 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -8,4 +8,5 @@ veggie2 = "carrots"
 def meal_choice (protein = "Tofu", veggie1, veggie2)
 puts "What a nutritional meal!"
 puts "A plate of #{protein} with #{veggie1} and #{veggie2}."
+puts "What a nutritional meal!"
 end
